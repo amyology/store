c84068c8c1fe03eea10876f9d355ec9d38851b13
Amy Chan <chamy09@gmail.com>
2016-05-12 21:26:35 -0500

    added forms

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,24 @@
 class ProductsController < ApplicationController
-  def home
+  def index
     @products =  Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    @product = Product.create(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image],
+      description: params[:description],
+      inventory: params[:inventory]
+    )
+    render 'show.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  get "/" => "products#home"
+  get '/' => 'products#index'
+  get '/store' => 'products#index'
+
+  get '/store/new' => 'products#new'
+  post '/store' => 'products#create'
+
+  get '/store/:id' => 'products#show'
+
 end
