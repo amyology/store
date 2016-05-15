c35e2dfe5956e0299d4b4a6d6c80b7e2b118903f
Amy Chan <chamy09@gmail.com>
2016-05-15 16:53:57 -0500

    added more css

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,6 +19,34 @@ class ProductsController < ApplicationController
       description: params[:description],
       inventory: params[:inventory]
     )
-    render 'show.html.erb'
+
+    flash[:success] = "Successfully created."
+    redirect_to "/store/#{@product.id}"
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image],
+      description: params[:description],
+      inventory: params[:inventory]
+    )
+
+    flash[:success] = "Successfully updated."
+    redirect_to "/store/#{@product.id}"
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    flash[:warning] = "Successfully deleted."
+    redirect_to "/"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   get '/store/:id' => 'products#show'
 
+  get '/store/:id/edit' => 'products#edit'
+  patch '/store/:id' => 'products#update'
+
+  delete '/store/:id' => 'products#destroy'
+
 end
