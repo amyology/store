4bbfa4e96ed5bd3c8e92be35e1b5c102be667b21
Amy Chan <chamy09@gmail.com>
2016-05-16 21:30:22 -0500

    added tax

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,21 @@
 class Product < ActiveRecord::Base
+
+TAX_RATE = 0.09
+
+def sale_message
+  if price < 11
+    "Discount item"
+  else
+    "On sale"
+  end
+end
+
+def tax
+  price * TAX_RATE
+end
+
+def total
+  price + (price * TAX_RATE)
+end
+
 end
